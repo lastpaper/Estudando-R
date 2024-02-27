3567bd9f04c0a153c14520c1f4cd9511346c4bca
PAPEL <159182926+lastpaper@users.noreply.github.com>
2024-02-27 15:35:47 -0300

    Update dia_7_fev.R

--- dia_7_fev.R
+++ dia_7_fev.R
@@ -3,20 +3,21 @@ solution <- function(s){
   stringi::stri_reverse(s)
 }
 solution("world")
-
+#-----------------------------------------------------
 
 # Função que multiplica por vetores por 2
 maps <- function(v) {
   v*2
 }
 maps(c(1,2,3))
-
+#-----------------------------------------------------
 
 # Função que converte numero para string
 number_to_string <- function(n) {
   as.character(n)
 }
 number_to_string(932)
+#-----------------------------------------------------
 
 # Função que encontra Impar ou Par
 even_or_odd <- function(n) {
@@ -27,7 +28,7 @@ even_or_odd <- function(n) {
   }
 }
 even_or_odd(9)
-
+#-----------------------------------------------------
 
 # Converte Booleano em String
 boolean_to_string <- function(b){
@@ -36,6 +37,7 @@ boolean_to_string <- function(b){
 }
 boolean_to_string(TRUE)
 boolean_to_string(FALSE)
+#-----------------------------------------------------
 
 # Converte Km/hora por Cm/segundo e desconsidera casas decimais
 cockroach_speed <- function(s){
@@ -44,6 +46,7 @@ cockroach_speed <- function(s){
 cockroach_speed(1.08)
 cockroach_speed(1.09)
 cockroach_speed(0)
+#-----------------------------------------------------
 
 # Verifica se a primeia letra começa com R
 are_you_playing_banjo <- function(name){
@@ -58,7 +61,7 @@ are_you_playing_banjo <- function(name){
 are_you_playing_banjo('Renato')
 are_you_playing_banjo('rodrigo')
 are_you_playing_banjo('Luiz')
-
+#-----------------------------------------------------
 
 # Soma somente os numeros positivos do array
 positive_sum <- function(vec){
@@ -71,12 +74,14 @@ positive_sum <- function(vec){
   return(resultado_da_soma)
 }
 positive_sum(c(-1,2,3,4,-5))
-             
+#-----------------------------------------------------
+
 # Remove os pontos de exclamação
 remove_exclamation_marks <- function(s){
   gsub('!', '', s)
 }
 remove_exclamation_marks("Hello World!!!!")
+#-----------------------------------------------------
 
 # Trasformar palavras de um vetor numa string
 smash <- function(words){
