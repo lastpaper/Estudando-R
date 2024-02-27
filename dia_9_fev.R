54a71a4c7cfb2e1648121f67e65d8796ffc6295f
PAPEL <159182926+lastpaper@users.noreply.github.com>
2024-02-27 15:36:03 -0300

    Update dia_9_fev.R

--- dia_9_fev.R
+++ dia_9_fev.R
@@ -6,8 +6,7 @@ is.vector(lista.de.arquivos)
 for(i in lista.de.arquivos){
   print(paste('Leia o arquivo: ', i))  
 }
-
-#------
+#-----------------------------------------------------
 # Valores menores que 5 são substituidos por 0 e valores iguais ou maiores são substituidos por 1
 fake_bin <- function(x){
   chartr("123456789", "000011111", x) 
